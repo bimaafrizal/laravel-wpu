988637f006afedaa2e3db5525ada082f5054385e
bimaafrizal <bimaafrizalmalna@student.uns.ac.id>
2022-03-04 16:36:46 +0700

    struktur folder routes & View

---
+++ instalasi/resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>About</title>
+</head>
+<body>
+    <h1>Ini halaman about</h1>
+    <h3><?= $name; ?></h3>
+    <h3><?= $email; ?></h3>
+    <script src="script.js"></script>
+    <img src="<?= $image; ?>" alt="CV Saya" width="300">
+</body>
+</html>
--- instalasi/routes/web.php
+++ instalasi/routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Bima Afrizal",
+        "email" => "bimaafrizalmalna@gmail.com",
+        "image" => "cv_saya.png"
+    ]);
+});
+Route::get('/blog', function () {
+    return view('blog');
+});
